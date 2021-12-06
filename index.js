214ed59854e1b6b0f54e9bd01f172b0448b26915
mohammad yaasir cheekoory <yaasir1997@gmail.com>
2021-12-06 14:39:46 +0400

    added css grid

---
+++ index.js
@@ -0,0 +1,6 @@
+const cards = document.querySelector('.cards');
+
+for (let index = 0; index < 9; index++) {
+    const cardHtmlClass = `<div class='card'>WEAR YOUR FUCKING MASK</div>`;
+    cards.insertAdjacentHTML('beforeend', cardHtmlClass);
+}
